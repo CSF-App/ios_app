006b9fb6a7dbba423d6302f3bcd8c92e1b592a19
Anibal <anibalramirezpadilla@live.com.mx>
2018-05-24 14:17:05 -0700

    I did the sidebarmenu
    https://www.youtube.com/watch?v=rafJcqqyS1E

--- CSF App/CSF App/ViewController.swift
+++ CSF App/CSF App/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CSF App
 //
-//  Created by Anibal Ramirez padilla on 5/22/18.
+//  Created by Anibal Ramirez padilla on 5/24/18.
 //  Copyright © 2018 CSF App. All rights reserved.
 //
 
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alertButton: UIBarButtonItem!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//call sideMenus
+        sideMenus()
+        customizeNavBar() 
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+//side menu method
+    func sideMenus(){
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+        
+        
+    }
+    
+    //custom color for menu
+    
+    func customizeNavBar(){
+        navigationController?.navigationBar.tintColor = UIColor(displayP3Red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 255/255, green: 90/255, blue: 30/255, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [kCTForegroundColorAttributeName: UIColor.white] as [NSAttributedStringKey : Any]
+        
+    }
+    
 }
-
