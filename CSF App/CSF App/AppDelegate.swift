faedcff6f20503ac5ddda2ee092e094bab4585a7
Sara Durkovich <SaraDurkovich@Olsens-MacBook-Pro.local>
2018-05-25 11:32:00 -0700

    Map SDK API Installed

--- CSF App/CSF App/AppDelegate.swift
+++ CSF App/CSF App/AppDelegate.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import GoogleMaps
+import GooglePlaces
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAEUPRjcwevsEVAuz136OxwbHA4iqKcb7w")
+        GMSPlacesClient.provideAPIKey("AIzaSyAEUPRjcwevsEVAuz136OxwbHA4iqKcb7w")
         return true
     }
 
